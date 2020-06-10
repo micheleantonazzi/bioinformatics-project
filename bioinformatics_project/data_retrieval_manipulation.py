d2830327d4e2b32ce4f64ea58894663581500772
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-10 10:22:12 +0200

    data_retrieval_manipulation.py: added function to download dataset

--- bioinformatics_project/data_retrieval_manipulation.py
+++ bioinformatics_project/data_retrieval_manipulation.py
@@ -9,12 +9,18 @@ def download_data(cell_line='HEK293', window_size=200):
         window_size=window_size
     )
 
+    print('Data obtained: promoters epigenomes and labels')
+
     enhancers_data, enhancers_labels = load_epigenomes(
         cell_line=cell_line,
         dataset="fantom",
         regions="enhancers",
         window_size=window_size
     )
+
+    print('Data obtained: enhancers epigenomes and labels')
+
+    return promoters_data, promoters_labels, enhancers_data, enhancers_labels
 
 
 if __name__ == "__main__":
