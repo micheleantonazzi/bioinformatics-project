1460a97a6eccfcf39b8f554d15cefa7350ea0c45
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-10 11:14:09 +0200

    data_retrieval_manipulation.py: added dictionary in download_epigenomic_data method

--- bioinformatics_project/data_retrieval_manipulation.py
+++ bioinformatics_project/data_retrieval_manipulation.py
@@ -2,23 +2,24 @@ from epigenomic_dataset import load_epigenomes
 
 
 def download_epigenomic_data(cell_line='HEK293', window_size=200):
-    promoters_data, promoters_labels = load_epigenomes(
+    promoters_epigenomic_data, promoters_labels = load_epigenomes(
         cell_line=cell_line,
         dataset="fantom",
         regions="promoters",
         window_size=window_size
     )
-
+    promoters_data = {'data': promoters_epigenomic_data, 'labels': promoters_labels}
     print('Data obtained: promoters epigenomes and labels')
 
-    enhancers_data, enhancers_labels = load_epigenomes(
+    enhancers_epigenomic_data, enhancers_labels = load_epigenomes(
         cell_line=cell_line,
         dataset="fantom",
         regions="enhancers",
         window_size=window_size
     )
+    enhancers_data = {'data': enhancers_epigenomic_data, 'labels': enhancers_labels}
 
     print('Data obtained: enhancers epigenomes and labels')
 
-    return promoters_data, promoters_labels, enhancers_data, enhancers_labels
+    return promoters_data, enhancers_data
 
