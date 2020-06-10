8aafd2d430123437959c67852da132a9bcd96a47
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-10 10:22:39 +0200

    test_pipeline.py: added test_download_data

---
+++ tests/test_pipeline.py
@@ -0,0 +1,9 @@
+import bioinformatics_project.data_retrieval_manipulation as data_retrieval
+
+
+def test_download_data():
+    promoters_data, promoters_labels, enhancers_data, enhancers_labels = data_retrieval.download_data()
+    assert promoters_data.size == 20681163
+    assert promoters_labels.size == 99909
+    assert enhancers_data.size == 13542561
+    assert enhancers_labels.size == 65423
