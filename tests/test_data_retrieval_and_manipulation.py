d758a8fc7237425e933faa3e627aa7192cfd665e
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-11 18:02:06 +0200

    test_data_retrieval_and_manipulation.py: fixed tests

--- tests/test_data_retrieval_and_manipulation.py
+++ tests/test_data_retrieval_and_manipulation.py
@@ -1,12 +1,11 @@
-import pytest
-
+from pytest import mark, fail
 from bioinformatics_project.data_retrieval_and_manipulation.data_checking import DataChecking
 from bioinformatics_project.data_retrieval_and_manipulation.data_retrieval import DataRetrieval
 
 data_retrieval = DataRetrieval()
 
 
-@pytest.mark.dependency()
+@mark.dependency()
 def test_load_promoters_epigenomic_data():
     promoters_data = data_retrieval.load_promoters_epigenomic_data()
     assert promoters_data == data_retrieval.get_promoters_data()
@@ -14,7 +13,7 @@ def test_load_promoters_epigenomic_data():
     assert len(data_retrieval.get_promoters_labels()) == 99909
 
 
-@pytest.mark.dependency()
+@mark.dependency()
 def test_load_enhancers_epigenomic_data():
     enhancers_data = data_retrieval.load_enhancers_epigenomic_data()
     assert enhancers_data == data_retrieval.get_enhancers_data()
@@ -22,9 +21,27 @@ def test_load_enhancers_epigenomic_data():
     assert len(data_retrieval.get_enhancers_epigenomic_data()) == 65423
 
 
-@pytest.mark.dependency(depends=['test_load_enhancers_epigenomic_data', 'test_load_promoters_epigenomic_data'])
+@mark.dependency()
+def test_load_genome_data():
+    genome = data_retrieval.load_genome_data()
+    assert len(genome) == 25
+
+
+@mark.dependency(depends=['test_load_promoters_epigenomic_data', 'test_load_genome_data'])
+def test_extract_promoters_sequence_data():
+    promoters_sequence_data = data_retrieval.extract_promoters_sequence_data(10)
+    assert len(promoters_sequence_data) == 10
+
+
+@mark.dependency(depends=['test_load_enhancers_epigenomic_data', 'test_load_genome_data'])
+def test_extract_enhancers_sequence_data():
+    enhancers_sequence_data = data_retrieval.extract_enhancers_sequence_data(5)
+    assert len(enhancers_sequence_data) == 5
+
+
+@mark.dependency(depends=['test_load_enhancers_epigenomic_data', 'test_load_promoters_epigenomic_data'])
 def test_check_sample_features_imbalance():
     try:
         DataChecking(data_retrieval).check_sample_features_imbalance()
     except:
-        pytest.fail('Unexpected exception')
+        fail('Unexpected exception')
