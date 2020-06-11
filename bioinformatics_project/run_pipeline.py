4f36b947264dcb99c0d8d9d3efc473145186eb16
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-11 15:49:55 +0200

    run_pipeline.py: deleted all

--- bioinformatics_project/run_pipeline.py
+++ bioinformatics_project/run_pipeline.py
@@ -1,8 +1,6 @@
-from bioinformatics_project.data_retrieval_and_manipulation.data_retrieval import DataRetrieval
 
-data_retrieval = DataRetrieval()
-data_retrieval.load_promoters_epigenomic_data()
-data_retrieval.load_enhancers_epigenomic_data()
-genome = data_retrieval.load_genome_data()
-data_retrieval.extract_promoters_sequence_data()
-data_retrieval.extract_enhancers_sequence_data()
+
+
+
+
+
