edc6fc493ac1242960cac918f666c31cd184fa1e
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-11 15:24:21 +0200

    run_pipeline.py: fixed typo

--- bioinformatics_project/run_pipeline.py
+++ bioinformatics_project/run_pipeline.py
@@ -1,7 +1,7 @@
 from bioinformatics_project.data_retrieval_and_manipulation.data_retrieval import DataRetrieval
 
 data_retrieval = DataRetrieval()
-data_retrieval.load_promoters_epogenomic_data()
+data_retrieval.load_promoters_epigenomic_data()
 data_retrieval.load_enhancers_epigenomic_data()
 genome = data_retrieval.load_genome_data()
 data_retrieval.extract_promoters_sequence_data()
