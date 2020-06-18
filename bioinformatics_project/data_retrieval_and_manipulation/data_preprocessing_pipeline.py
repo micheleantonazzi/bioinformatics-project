6c812025cbd0fb1124d5ca6d6e2df904d7bb2252
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-18 15:45:59 +0200

    data_preprocessing_pipeline.py: now the dataRetrieval object is a class attribute

--- bioinformatics_project/data_retrieval_and_manipulation/data_preprocessing_pipeline.py
+++ bioinformatics_project/data_retrieval_and_manipulation/data_preprocessing_pipeline.py
@@ -9,74 +9,70 @@ from bioinformatics_project.data_retrieval_and_manipulation.data_retrieval impor
 class DataPreprocessingPipeline:
     FOLDER_V1: str = 'preprocessed_data_v1'
     FOLDER_V2: str = 'preprocessed_data_v2'
-    def execute_v1(self) -> DataRetrieval:
-        data_retrieval = DataRetrieval()
-        data_retrieval.load_promoters_epigenomic_data()
-        data_retrieval.load_enhancers_epigenomic_data()
-        data_retrieval.load_genome_data()
-        data_retrieval.extract_promoters_sequence_data()
-        data_retrieval.extract_enhancers_sequence_data()
 
-        if data_retrieval.check_exists_data_preprocessed(DataPreprocessingPipeline.FOLDER_V1):
+    def __init__(self, data: DataRetrieval):
+        self._data = data
+
+    def execute_v1(self) -> DataRetrieval:
+        self._data.load_promoters_epigenomic_data()
+        self._data.load_enhancers_epigenomic_data()
+
+        if self._data.check_exists_data_preprocessed(DataPreprocessingPipeline.FOLDER_V1):
             print(colored('Data have been already preprocessed', 'green'))
-            data_retrieval.load_epigenomic_data_from_csv(DataPreprocessingPipeline.FOLDER_V1)
+            self._data.load_epigenomic_data_from_csv(DataPreprocessingPipeline.FOLDER_V1)
         else:
             print(colored('Data have not been already preprocessed, starting preprocess procedure', 'red'))
-            promoters_features_number = len(data_retrieval.get_promoters_epigenomic_data().columns)
-            enhancers_features_number = len(data_retrieval.get_enhancers_epigenomic_data().columns)
-            data_preprocessing = DataPreprocessing(data_retrieval)
+            promoters_features_number = len(self._data.get_promoters_epigenomic_data().columns)
+            enhancers_features_number = len(self._data.get_enhancers_epigenomic_data().columns)
+            data_preprocessing = DataPreprocessing(self._data)
             data_preprocessing.fill_nan_data()
             data_preprocessing.drop_constant_features()
             data_preprocessing.apply_z_scoring()
             uncorrelated = data_preprocessing.apply_pearson_spearman_correlation()
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
+            self._data.remove_uncorrelated_features(uncorrelated)
             uncorrelated = data_preprocessing.apply_mic()
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
+            self._data.remove_uncorrelated_features(uncorrelated)
             uncorrelated, _ = data_preprocessing.apply_pearson_for_features_correlation()
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
+            self._data.remove_uncorrelated_features(uncorrelated)
             uncorrelated = data_preprocessing.apply_boruta(200)
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
-            data_retrieval.save_epigenomic_data_to_csv(DataPreprocessingPipeline.FOLDER_V1)
+            self._data.remove_uncorrelated_features(uncorrelated)
+            self._data.save_epigenomic_data_to_csv(DataPreprocessingPipeline.FOLDER_V1)
             print(colored(f'The initial amount of features for promoters was {promoters_features_number}, '
-                          f'after preprocessing it is {len(data_retrieval.get_promoters_epigenomic_data().columns)}', 'green'))
+                          f'after preprocessing it is {len(self._data.get_promoters_epigenomic_data().columns)}', 'green'))
             print(colored(f'The initial amount of features for enhancers was {enhancers_features_number}, '
-                          f'after preprocessing it is {len(data_retrieval.get_enhancers_epigenomic_data().columns)}', 'green'))
+                          f'after preprocessing it is {len(self._data.get_enhancers_epigenomic_data().columns)}', 'green'))
 
         print(colored('Applied preprocessing version 1', 'red'))
-        return data_retrieval
+        return self._data
 
     def execute_v2(self) -> DataRetrieval:
-        data_retrieval = DataRetrieval()
-        data_retrieval.load_promoters_epigenomic_data()
-        data_retrieval.load_enhancers_epigenomic_data()
-        data_retrieval.load_genome_data()
-        data_retrieval.extract_promoters_sequence_data()
-        data_retrieval.extract_enhancers_sequence_data()
+        self._data.load_promoters_epigenomic_data()
+        self._data.load_enhancers_epigenomic_data()
 
-        if data_retrieval.check_exists_data_preprocessed(DataPreprocessingPipeline.FOLDER_V2):
+        if self._data.check_exists_data_preprocessed(DataPreprocessingPipeline.FOLDER_V2):
             print(colored('Data have been already preprocessed', 'green'))
-            data_retrieval.load_epigenomic_data_from_csv(DataPreprocessingPipeline.FOLDER_V2)
+            self._data.load_epigenomic_data_from_csv(DataPreprocessingPipeline.FOLDER_V2)
         else:
             print(colored('Data have not been already preprocessed, starting preprocess procedure', 'red'))
-            promoters_features_number = len(data_retrieval.get_promoters_epigenomic_data().columns)
-            enhancers_features_number = len(data_retrieval.get_enhancers_epigenomic_data().columns)
-            data_preprocessing = DataPreprocessing(data_retrieval)
+            promoters_features_number = len(self._data.get_promoters_epigenomic_data().columns)
+            enhancers_features_number = len(self._data.get_enhancers_epigenomic_data().columns)
+            data_preprocessing = DataPreprocessing(self._data)
             data_preprocessing.fill_nan_data()
             data_preprocessing.drop_constant_features()
             data_preprocessing.apply_z_scoring()
             uncorrelated = data_preprocessing.apply_pearson_spearman_correlation()
             uncorrelated = data_preprocessing.apply_mic_on_selected_features(uncorrelated)
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
+            self._data.remove_uncorrelated_features(uncorrelated)
             uncorrelated, _ = data_preprocessing.apply_pearson_for_features_correlation()
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
+            self._data.remove_uncorrelated_features(uncorrelated)
             uncorrelated = data_preprocessing.apply_boruta(300)
-            data_retrieval.remove_uncorrelated_features(uncorrelated)
-            data_retrieval.save_epigenomic_data_to_csv(DataPreprocessingPipeline.FOLDER_V2)
+            self._data.remove_uncorrelated_features(uncorrelated)
+            self._data.save_epigenomic_data_to_csv(DataPreprocessingPipeline.FOLDER_V2)
             print(colored(f'The initial amount of features for promoters was {promoters_features_number}, '
-                          f'after preprocessing it is {len(data_retrieval.get_promoters_epigenomic_data().columns)}', 'green'))
+                          f'after preprocessing it is {len(self._data.get_promoters_epigenomic_data().columns)}', 'green'))
             print(colored(f'The initial amount of features for enhancers was {enhancers_features_number}, '
-                          f'after preprocessing it is {len(data_retrieval.get_enhancers_epigenomic_data().columns)}', 'green'))
+                          f'after preprocessing it is {len(self._data.get_enhancers_epigenomic_data().columns)}', 'green'))
 
         print(colored('Applied preprocessing version 2', 'red'))
-        return data_retrieval
+        return self._data
 
