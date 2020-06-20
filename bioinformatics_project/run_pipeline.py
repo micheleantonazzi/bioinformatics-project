9d1128ed8d1d877322488c45e379dcb8c9fae0f2
micheleantonazzi <micheleantonazzi@gmail.com>
2020-06-20 14:17:09 +0200

    fixed pipeline

--- bioinformatics_project/run_pipeline.py
+++ bioinformatics_project/run_pipeline.py
@@ -1,8 +1,14 @@
 from bioinformatics_project.experiment.experiment_executor import ExperimentExecutor
+from bioinformatics_project.data_retrieval_and_manipulation.data_retrieval import DataRetrieval
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow
 
 
-ret = ExperimentExecutor().execute_promoters_epigenomic_experiment()
+executor = ExperimentExecutor()
+ret = executor.execute_promoters_epigenomic_experiment(DataRetrieval.KEY_PROMOTERS)
+executor.print_results(ret)
+
+ret = executor.execute_promoters_epigenomic_experiment(DataRetrieval.KEY_ENHANCERS)
+executor.print_results(ret)
 
